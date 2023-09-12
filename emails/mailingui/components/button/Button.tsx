cb5631c6ee6e1442ed5a9a7df4608416f8257006
Hector Sosa <hello@hectorsosa.me>
2023-09-12 11:56:51 +0200

    Initial commit

---
+++ emails/mailingui/components/button/Button.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+import { Button as ReactEmailButton } from "@react-email/components";
+
+import { Theme } from "../../themes";
+import { cx } from "../../utils";
+
+export const Button: React.FC<
+  React.ComponentPropsWithoutRef<"button"> & {
+    href: string;
+    secondary?: boolean;
+    destructive?: boolean;
+    fullWidth?: boolean;
+    rounded?: boolean;
+    compact?: boolean;
+    theme?: Theme;
+  }
+> = ({
+  href,
+  secondary,
+  destructive,
+  fullWidth,
+  rounded,
+  compact,
+  theme,
+  ...props
+}) => (
+  <ReactEmailButton
+    pY={16}
+    pX={24}
+    href={href}
+    style={cx(
+      [
+        "global",
+        "text",
+        !secondary && !destructive && "primary",
+        secondary && "secondary",
+        destructive && "destructive",
+        rounded && "rounded",
+        compact && "compact",
+        fullWidth && {
+          width: "100%",
+          boxSizing: "border-box",
+          textAlign: "center",
+        },
+        props.style,
+      ],
+      { theme }
+    )}
+  >
+    {props.children}
+  </ReactEmailButton>
+);
---
+++ emails/mailingui/components/index.ts
@@ -0,0 +1,8 @@
+
+export { Badge } from "./badge/Badge";
+
+export { Button } from "./button/Button";
+
+export { Typography,H1,H2,H3,H4,P,Blockquote,HR,Code,Link,UL,OL,LI,Img } from "./typography/Typography";
+
+export { Markdown,getMDXComponents } from "./markdown/Markdown";
